e5b4fd883d1f0e4400fa18e9ad02324295241109
nik-kotine <mswv280407@gmail.com>
2026-06-08 19:07:20 -0500

    feat: add keyboard support for clicker

--- clicker/js/input.js
+++ clicker/js/input.js
@@ -5,14 +5,19 @@
 const Input = (() => {
     const btn = document.getElementById('click-btn');
 
+	function triggerClick() {
+		const rect = btn.getBoundingClientRect();
+		Engine.handleClick(
+			rect.left + rect.width / 2,
+			rect.top + rect.height / 2,
+			false
+		)
+	}
+
     function start() {
         btn.addEventListener('click', (e) => {
             e.preventDefault();
-
-            const rect = btn.getBoundingClientRect();
-            const x = rect.left + rect.width / 2;
-            const y = rect.top + rect.height / 2;
-            Engine.handleClick(x, y, false);
+			triggerClick();
         });
 
         btn.addEventListener('touchstart', (e) => {
@@ -21,6 +26,12 @@ const Input = (() => {
             Engine.handleClick(t.clientX, t.clientY, false);
         }, { passive: false });
 
+		document.addEventListener('keydown', (e) => {
+			if (/^[a-zA-Z]$/.test(e.key)) {
+				e.preventDefault();
+				triggerClick();
+			}
+		});
         btn.classList.add('idle-bounce');
         btn.addEventListener('mousedown', () => btn.classList.remove('idle-bounce'));
         btn.addEventListener('mouseup', () => btn.classList.add('idle-bounce'));
